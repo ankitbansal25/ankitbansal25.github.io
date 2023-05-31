280a2e53a38dce5c9d2de4296550273679c9f05a
Ankit Bansal <abnitkkr@gmail.com>
2023-05-31 21:31:30 +0530

    chore: copyplugin added

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
   entry: './index.tsx',
   output: {
@@ -36,6 +37,11 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
     template: path.resolve(__dirname, "index.html"),
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'CNAME' }
+      ]
     }),
   ],
 }
